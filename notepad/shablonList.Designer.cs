1d7ce17557a19e08482b6004aed99ef59fdc8e27
FastStim <student2063@yandex.ru>
2017-06-04 18:49:12 +0300

    Last fix end

---
+++ notepad/shablonList.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace notepad
+{
+    partial class shablonList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.notes = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // notes
+            // 
+            this.notes.AutoScroll = true;
+            this.notes.Dock = System.Windows.Forms.DockStyle.Top;
+            this.notes.Location = new System.Drawing.Point(0, 0);
+            this.notes.Name = "notes";
+            this.notes.Size = new System.Drawing.Size(380, 458);
+            this.notes.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 464);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Отменить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(281, 464);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(87, 37);
+            this.add.TabIndex = 4;
+            this.add.Text = "Добавить";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // shablonList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 513);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.notes);
+            this.Name = "shablonList";
+            this.Text = "Список шаблонов";
+            this.Activated += new System.EventHandler(this.shablonList_Activated);
+            this.Load += new System.EventHandler(this.shablonList_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel notes;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button add;
+    }
+}
---
+++ notepad/shablonList.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace notepad
+{
+    public partial class shablonList : Form
+    {
+        private sql note = new sql("bazadannih.db");
+        public static bool refresh = false;
+
+        public shablonList()
+        {
+            InitializeComponent();
+        }
+
+        private void shablonList_Load(object sender, EventArgs e)
+        {
+            int size = note.getCountShablon();
+            shablons[] shablon = note.getShablon();
+
+            createFormObject(size, shablon);
+        }
+
+        private void createFormObject(int size, shablons[] answer)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                notes.Controls.Add(new GroupBox() { Name = "note" + i, Text = "Шаблон " + answer[i].id.ToString(), Location = new Point(10, i * 300 + 10), Size = new Size(350, 300) });
+
+                Control[] temp = notes.Controls.Find("note" + i, true);
+
+                string sex = "Мужской";
+                if (answer[i].sex == 1)
+                    sex = "Женский";
+                else if (answer[i].sex == 2)
+                    sex = "Универсальный";
+                temp[0].Controls.Add(new Label() { Name = "sex" + i, Text = sex, Location = new Point(20, 22), Size = new Size(90, 20) });
+                temp[0].Controls.Add(new TextBox() {BackColor = SystemColors.Control, ReadOnly = true,  Multiline = true, BorderStyle = BorderStyle.None, Name = "text" + i, Text = answer[i].text, Location = new Point(20, 50), Size = new Size(310, 200) });
+
+                temp[0].Controls.Add(new Button() { Name = "editButton_" + answer[i].id, Text = "Изменить", Location = new Point(120, 20), Size = new Size(90, 20) });
+                temp[0].Controls.Add(new Button() { Name = "deleteButton_" + answer[i].id, Text = "Удалить", Location = new Point(220, 20), Size = new Size(90, 20) });
+
+                Control[] temp_l = temp[0].Controls.Find("editButton_" + answer[i].id, true);
+                temp_l[0].Click += new EventHandler(edit_Click);
+                temp_l = temp[0].Controls.Find("deleteButton_" + answer[i].id, true);
+                temp_l[0].Click += new EventHandler(delete_Click);
+            }
+
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            string[] splits = ((Button)sender).Name.Split('_');
+            int id = Convert.ToInt32(splits[1]);
+
+            string message = "Вы уверены что хотите удалить шаблон?";
+            string caption = "Удаление шаблона";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            DialogResult result;
+
+            result = MessageBox.Show(message, caption, buttons);
+
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                note.deleteShablon(id);
+
+                refresh = true;
+                refreshForm();
+            }
+        }
+
+        private void edit_Click(object sender, EventArgs e)
+        {
+            shablonEdit shablonEdit = new shablonEdit();
+            string[] splits = ((Button)sender).Name.Split('_');
+            int id = Convert.ToInt32(splits[1]);
+            shablonEdit.id = id;
+            shablonEdit.Show();
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            shablonEdit shablonEdit = new shablonEdit();
+            shablonEdit.id = 0;
+            shablonEdit.Show();
+        }
+
+        private void refreshForm()
+        {
+            if (refresh == true)
+            {
+                notes.Controls.Clear();
+
+                int size = note.getCountShablon();
+                shablons[] shablon = note.getShablon();
+
+                createFormObject(size, shablon);
+
+                refresh = false;
+            }
+        }
+
+        private void shablonList_Activated(object sender, EventArgs e)
+        {
+            refreshForm();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+}
